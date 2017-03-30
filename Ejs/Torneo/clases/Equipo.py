740833e4aef104d110079269b86bbd7c6bf683a4
Po <santyazr@gmail.com>
2017-03-30 11:58:50 -0300

    Mejor tarde que nunca

---
+++ Ejs/Torneo/clases/Equipo.py
@@ -0,0 +1,9 @@
+class Equipo (object):
+    nombre=""
+    disp=[]
+    cap=""
+    list_jugadore=[]
+    barrio=""
+
+    def set_agreJug(self,j):
+        self.j=str(nombre)
---
+++ Ejs/Torneo/clases/Jugador.py
@@ -0,0 +1,11 @@
+class Alumno (object):
+    nombre=""
+    fecha_nac=None
+    num_cami=None
+
+    def set_nombre(self,n)
+        self.n=str(nombre)
+    def set_fechaNac(self, f):
+        self.f=int(fecha_nac)
+    def set_numCam(self,N):
+        self.N=int(num_cami)
